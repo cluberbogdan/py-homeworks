58c7c69cb4638c956405977a7ec7c4433a34e1a1
b.yeremichuk <cluberbogdan@gmail.com>
2023-09-15 11:04:12 +0300

    add edited task

--- Lesson-15/lesson-15.sql
+++ Lesson-15/lesson-15.sql
@@ -1,46 +1,67 @@
 CREATE TABLE `users` (
-  `user_id` integer PRIMARY KEY,
-  `username` integer,
+  `user_id` int PRIMARY KEY,
+  `username` varchar(255),
   `email` varchar(255),
   `password_hash` varchar(255),
   `user_type` varchar(255)
 );
 
 CREATE TABLE `rooms` (
-  `room_id` integer PRIMARY KEY,
-  `host_id` integer,
+  `room_id` int PRIMARY KEY,
+  `host_id` int,
   `room_name` varchar(255),
-  `max_residents` varchar(255),
-  `price_per_night` integer,
-  `has_ac` varchar(255),
-  `has_refrigerator` varchar(255)
+  `max_residents` int,
+  `price_per_night` decimal,
+  `has_ac` boolean,
+  `has_refrigerator` boolean
 );
 
 CREATE TABLE `reservations` (
-  `reservation_id` integer PRIMARY KEY,
-  `guest_id` varchar(255),
-  `room_id` integer,
+  `reservation_id` int PRIMARY KEY,
+  `guest_id` int,
+  `room_id` int,
+  `max_residence` int,
   `check_in_date` date,
   `check_out_date` date,
-  `total_price` integer
+  `total_price` decimal
 );
 
 CREATE TABLE `reviews` (
-  `review_id` integer PRIMARY KEY,
-  `guest_id` varchar(255),
-  `room_id` integer,
-  `rating` integer,
+  `review_id` int PRIMARY KEY,
+  `reservation_id` int,
+  `rating` int,
   `review_text` text,
   `review_date` date,
-  `total_price` integer
+  `total_price` decimal
+);
+
+CREATE TABLE `paymentTransactions` (
+  `transaction_id` int PRIMARY KEY,
+  `guest_id` int,
+  `room_id` int,
+  `payment_date` date,
+  `total_price` decimal,
+  `payment_status` varchar(255)
+);
+
+CREATE TABLE `hostReviews` (
+  `host_review_id` int PRIMARY KEY,
+  `guest_id` int,
+  `host_id` int,
+  `review_text` text,
+  `review_date` date
 );
 
 ALTER TABLE `users` ADD FOREIGN KEY (`user_id`) REFERENCES `rooms` (`host_id`);
 
 ALTER TABLE `users` ADD FOREIGN KEY (`user_id`) REFERENCES `reservations` (`guest_id`);
 
-ALTER TABLE `rooms` ADD FOREIGN KEY (`room_id`) REFERENCES `reviews` (`room_id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`user_id`) REFERENCES `reviews` (`guest_id`);
-
 ALTER TABLE `rooms` ADD FOREIGN KEY (`room_id`) REFERENCES `reservations` (`room_id`);
+
+ALTER TABLE `reservations` ADD FOREIGN KEY (`reservation_id`) REFERENCES `reviews` (`reservation_id`);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`user_id`) REFERENCES `hostReviews` (`guest_id`);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`user_id`) REFERENCES `paymentTransactions` (`guest_id`);
+
+ALTER TABLE `rooms` ADD FOREIGN KEY (`room_id`) REFERENCES `paymentTransactions` (`room_id`);
